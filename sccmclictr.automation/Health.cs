c316daa98442632c48c8548d2e132112f9993e97
rzander_cp <SND\rzander_cp>
2012-09-09 11:12:02 +0000

    Client Health: Get and Set DCOM Permission function implemented

--- sccmclictr.automation/Health.cs
+++ sccmclictr.automation/Health.cs
@@ -37,7 +37,7 @@ namespace sccmclictr.automation.functions
         }
 
         /// <summary>
-        /// Verify WMI Repository (winmgmt /verifyrepository)
+        /// Verify WMI Repository (winmgmt /verifyrepository).
         /// </summary>
         /// <returns>Command results as string</returns>
         public string WMIVerifyRepository()
@@ -53,7 +53,7 @@ namespace sccmclictr.automation.functions
         }
 
         /// <summary>
-        /// Performs a consistency check on the WMI repository (winmgmt /salvagerepository)
+        /// Performs a consistency check on the WMI repository (winmgmt /salvagerepository).
         /// </summary>
         /// <returns>Command results as string</returns>
         public string WMISalvageRepository()
@@ -64,7 +64,7 @@ namespace sccmclictr.automation.functions
         }
 
         /// <summary>
-        /// The repository is reset to the initial state when the operating system is first installed (winmgmt /resetrepository)
+        /// The repository is reset to the initial state when the operating system is first installed (winmgmt /resetrepository).
         /// </summary>
         /// <returns>Command results as string</returns>
         public string WMIResetRepository()
@@ -72,6 +72,87 @@ namespace sccmclictr.automation.functions
             string sResult = base.GetStringFromPS("winmgmt /resetrepository", true);
 
             return sResult;
+        }
+
+        /// <summary>
+        /// Registers the system performance libraries with WMI (winmgmt /resyncperf).
+        /// </summary>
+        /// <returns>Command results as string</returns>
+        public string WMIRegPerfLibraries()
+        {
+            string sResult = base.GetStringFromPS("winmgmt /resyncperf", true);
+
+            return sResult;
+        }
+
+        /// <summary>
+        /// Rgister DLL using Regsvr32
+        /// </summary>
+        /// <param name="File"></param>
+        /// <returns></returns>
+        public string RegsiertDLL(string File)
+        {
+            string sResult = base.GetStringFromPS(string.Format("regsvr32.exe /s \"{0}\"", Environment.ExpandEnvironmentVariables(File)), true);
+
+            return sResult;
+        }
+
+        /// <summary>
+        /// State if DCOM is enabled (HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Ole:EnableDCOM)
+        /// </summary>
+        public bool EnableDCOM
+        {
+            get
+            {
+                TimeSpan toldCacheTime = base.cacheTime;
+
+                //Cache for 30 seconds
+                base.cacheTime = new TimeSpan(0, 0, 30);
+                string sResult = base.GetStringFromPS("(get-ItemProperty \"HKLM:\\SOFTWARE\\Microsoft\\Ole\").\"EnableDCOM\"");
+                base.cacheTime = toldCacheTime;
+                if (string.Compare(sResult, "Y", true) == 0)
+                    return true;
+                else
+                    return false;
+            }
+
+            set
+            {
+                string sValue = value ? "Y" : "N";
+                string sResult = base.GetStringFromPS(string.Format("set-ItemProperty -Path \"HKLM:\\SOFTWARE\\Microsoft\\Ole\" -Name \"EnableDCOM\" -Value \"{0}\"", sValue));
+            }
+        }
+
+        /// <summary>
+        /// Get the DCOM Permission ACL from a Binary Registry Key.
+        /// </summary>
+        /// <param name="HKLMKey">HKLM Key like "software\microsoft\ole"</param>
+        /// <param name="RegValue">Registry Value that contains the DCOM Permission like "MachineLaunchRestriction"</param>
+        /// <returns></returns>
+        public string GetDCOMPerm(string HKLMKey, string RegValue)
+        {
+            TimeSpan toldCacheTime = base.cacheTime;
+
+            //Cache for 30 seconds
+            base.cacheTime = new TimeSpan(0, 0, 30);
+            string sResult = base.GetStringFromPS(string.Format(Properties.Settings.Default.PSGetDCOMPerm, HKLMKey, RegValue));
+            
+            base.cacheTime = toldCacheTime;
+            return sResult;
+        }
+
+        /// <summary>
+        /// Set the DCOM Permission ACL to a Binary Registry Key.
+        /// O:BAG:BAD:(A;;CCDCSW;;;WD)(A;;CCDCLCSWRP;;;BA)(A;;CCDCLCSWRP;;;LU)(A;;CCDCLCSWRP;;;S-1-5-32-562)
+        /// </summary>
+        /// <param name="HKLMKey">HKLM Key like "software\microsoft\ole"</param>
+        /// <param name="RegValue">Registry Value that contains the DCOM Permission like "MachineLaunchRestriction"</param>
+        /// <param name="ACL">ACL like "O:BAG:BAD:(A;;CCDCSW;;;WD)(A;;CCDCLCSWRP;;;BA)(A;;CCDCLCSWRP;;;LU)(A;;CCDCLCSWRP;;;S-1-5-32-562)"</param>
+        /// <returns></returns>
+        public string SetDCOMPerm(string HKLMKey, string RegValue, string ACL)
+        {
+            string sResult = base.GetStringFromPS(string.Format(Properties.Settings.Default.PSSetDCOMPerm, HKLMKey, RegValue, ACL), true);
+            return "";
         }
     }
 }
