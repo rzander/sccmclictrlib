06e6cb36ad471d77f7d2a38cde9f5652c55178ba
rzander_cp <SND\rzander_cp>
2013-08-08 18:36:32 +0000

    ReRun Advertisements function implemented.

--- sccmclictr.automation/SoftwareDistribution.cs
+++ sccmclictr.automation/SoftwareDistribution.cs
@@ -66,6 +66,25 @@ namespace sccmclictr.automation.functions
             }
         }
 
+        public List<CCM_SoftwareBase> Software
+        {
+            get
+            {
+                List<CCM_SoftwareBase> lApps = new List<CCM_SoftwareBase>();
+                List<PSObject> oObj = GetObjects(@"ROOT\ccm\ClientSDK", "SELECT * FROM CCM_SoftwareBase");
+                foreach (PSObject PSObj in oObj)
+                {
+                    //Get AppDTs sub Objects
+                    CCM_SoftwareBase oApp = new CCM_SoftwareBase(PSObj);
+
+                    oApp.remoteRunspace = remoteRunspace;
+                    oApp.pSCode = pSCode;
+                    lApps.Add(oApp);
+                }
+                return lApps;
+            }
+        }
+
         /// <summary>
         /// List of the System Execution History (only Machine based !)
         /// </summary>
@@ -154,6 +173,80 @@ namespace sccmclictr.automation.functions
             public UInt32? PercentComplete { get; set; }
             public String Publisher { get; set; }
             public UInt32? Type { get; set; }
+
+            /// <summary>
+            /// Transalated EvaluationState into text from MSDN (http://msdn.microsoft.com/en-us/library/jj874280.aspx)
+            /// </summary>
+            public string EvaluationStateText
+            {
+                get
+                {
+                    switch (EvaluationState)
+                    {
+                        case 0:
+                            return "No state information is available.";
+                        case 1:
+                            return "Application is enforced to desired/resolved state.";
+                        case 2:
+                            return "Application is not required on the client.";
+                        case 3:
+                            return "Application is available for enforcement (install or uninstall based on resolved state). Content may/may not have been downloaded.";
+                        case 4:
+                            return "Application last failed to enforce (install/uninstall).";
+                        case 5:
+                            return "Application is currently waiting for content download to complete.";
+                        case 6:
+                            return "Application is currently waiting for content download to complete.";
+                        case 7:
+                            return "Application is currently waiting for its dependencies to download.";
+                        case 8:
+                            return "Application is currently waiting for a service (maintenance) window.";
+                        case 9:
+                            return "Application is currently waiting for a previously pending reboot.";
+                        case 10:
+                            return "Application is currently waiting for serialized enforcement.";
+                        case 11:
+                            return "Application is currently enforcing dependencies.";
+                        case 12:
+                            return "Application is currently enforcing.";
+                        case 13:
+                            return "Application install/uninstall enforced and soft reboot is pending.";
+                        case 14:
+                            return "Application installed/uninstalled and hard reboot is pending.";
+                        case 15:
+                            return "Update is available but pending installation.";
+                        case 16:
+                            return "Application failed to evaluate.";
+                        case 17:
+                            return "Application is currently waiting for an active user session to enforce.";
+                        case 18:
+                            return "Application is currently waiting for all users to logoff.";
+                        case 19:
+                            return "Application is currently waiting for a user logon.";
+                        case 20:
+                            return "Application in progress, waiting for retry.";
+                        case 21:
+                            return "Application is waiting for presentation mode to be switched off.";
+                        case 22:
+                            return "Application is pre-downloading content (downloading outside of install job).";
+                        case 23:
+                            return "Application is pre-downloading dependent content (downloading outside of install job).";
+                        case 24:
+                            return "Application download failed (downloading during install job).";
+                        case 25:
+                            return "Application pre-downloading failed (downloading outside of install job).";
+                        case 26:
+                            return "Download success (downloading during install job).";
+                        case 27:
+                            return "Post-enforce evaluation.";
+                        case 28:
+                            return "Waiting for network connectivity.";
+                        default:
+                            return "Unknown state information.";
+
+                    }
+                }
+            }
             #endregion
 
             #region Methods
@@ -325,80 +418,6 @@ namespace sccmclictr.automation.functions
             public DateTime? StartTime { get; set; }
             public String SupersessionState { get; set; }
             public Boolean? UserUIExperience { get; set; }
-
-            /// <summary>
-            /// Transalated EvaluationState into text from MSDN (http://msdn.microsoft.com/en-us/library/jj874280.aspx)
-            /// </summary>
-            public string EvaluationStateText
-            {
-                get
-                {
-                    switch (EvaluationState)
-                    {
-                        case 0:
-                            return "No state information is available.";
-                        case 1:
-                            return "Application is enforced to desired/resolved state.";
-                        case 2:
-                            return "Application is not required on the client.";
-                        case 3:
-                            return "Application is available for enforcement (install or uninstall based on resolved state). Content may/may not have been downloaded.";
-                        case 4:
-                            return "Application last failed to enforce (install/uninstall).";
-                        case 5:
-                            return "Application is currently waiting for content download to complete.";
-                        case 6:
-                            return "Application is currently waiting for content download to complete.";
-                        case 7:
-                            return "Application is currently waiting for its dependencies to download.";
-                        case 8:
-                            return "Application is currently waiting for a service (maintenance) window.";
-                        case 9:
-                            return "Application is currently waiting for a previously pending reboot.";
-                        case 10:
-                            return "Application is currently waiting for serialized enforcement.";
-                        case 11:
-                            return "Application is currently enforcing dependencies.";
-                        case 12:
-                            return "Application is currently enforcing.";
-                        case 13:
-                            return "Application install/uninstall enforced and soft reboot is pending.";
-                        case 14:
-                            return "Application installed/uninstalled and hard reboot is pending.";
-                        case 15:
-                            return "Update is available but pending installation.";
-                        case 16:
-                            return "Application failed to evaluate.";
-                        case 17:
-                            return "Application is currently waiting for an active user session to enforce.";
-                        case 18:
-                            return "Application is currently waiting for all users to logoff.";
-                        case 19:
-                            return "Application is currently waiting for a user logon.";
-                        case 20:
-                            return "Application in progress, waiting for retry.";
-                        case 21:
-                            return "Application is waiting for presentation mode to be switched off.";
-                        case 22:
-                            return "Application is pre-downloading content (downloading outside of install job).";
-                        case 23:
-                            return "Application is pre-downloading dependent content (downloading outside of install job).";
-                        case 24:
-                            return "Application download failed (downloading during install job).";
-                        case 25:
-                            return "Application pre-downloading failed (downloading outside of install job).";
-                        case 26:
-                            return "Download success (downloading during install job).";
-                        case 27:
-                            return "Post-enforce evaluation.";
-                        case 28:
-                            return "Waiting for network connectivity.";
-                        default:
-                            return "Unknown state information.";
-
-                    }
-                }
-            }
 
             /*
             public CCM_AppDeploymentType[] AppDTs
@@ -886,6 +905,10 @@ namespace sccmclictr.automation.functions
 
             #region Methods
 
+            /// <summary>
+            /// CCM_Scheduler_ScheduledMessage object
+            /// </summary>
+            /// <returns></returns>
             public CCM_Scheduler_ScheduledMessage _ScheduledMessage()
             {
                 XmlDocument xDoc = new XmlDocument();
@@ -903,6 +926,36 @@ namespace sccmclictr.automation.functions
                     catch { }
                 }
                 return null;
+            }
+
+            /// <summary>
+            /// Run (trigger) an advertisement
+            /// </summary>
+            public void TriggerSchedule(Boolean enforce)
+            {
+                string sSchedule = _ScheduledMessage().ScheduledMessageID;
+
+                if (enforce)
+                {
+                    string sPrgReq = PRG_Requirements;
+                    sPrgReq = sPrgReq.Replace("<OverrideServiceWindows>FALSE</OverrideServiceWindows>", "<OverrideServiceWindows>TRUE</OverrideServiceWindows>");
+                    sPrgReq = sPrgReq.Replace("\r", "");
+                    sPrgReq = sPrgReq.Replace("\n", "");
+                    sPrgReq = sPrgReq.Replace("\t", "");
+                    oNewBase.SetProperty(this.__NAMESPACE + ":" + this.__RELPATH.Replace("\"", "'"), "ADV_RepeatRunBehavior", "RerunAlways");
+                    oNewBase.SetProperty(this.__NAMESPACE + ":" + this.__RELPATH.Replace("\"", "'"), "ADV_MandatoryAssignments", "$True");
+                    oNewBase.SetProperty(this.__NAMESPACE + ":" + this.__RELPATH.Replace("\"", "'"), "PRG_Requirements", "\"" + sPrgReq + "\"");
+
+                    this.ADV_RepeatRunBehavior = "RerunAlways";
+                    this.ADV_MandatoryAssignments = true;
+                    this.PRG_Requirements = sPrgReq;
+
+                    //Wait 2s and hope that to policy is updated...
+                    System.Threading.Thread.Sleep(2000); 
+                }
+
+                //this.oNewBase.CallClassMethod(@"root\ccm\ClientSDK:CCM_ProgramsManager", "ExecuteProgram", "'" + PRG_ProgramID + "', '" + PKG_PackageID + "'");
+                this.oNewBase.CallClassMethod(@"ROOT\ccm:SMS_Client", "TriggerSchedule", "'" + sSchedule + "'");
             }
 
             #endregion
