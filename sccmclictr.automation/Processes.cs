bf6f4dd5e2828028c370ea43275ff03fd57a2a01
mwhittle_cp <SND\mwhittle_cp>
2014-02-25 11:35:58 +0000

    Updated XML comments in Process.cs

--- sccmclictr.automation/Processes.cs
+++ sccmclictr.automation/Processes.cs
@@ -21,6 +21,9 @@ using System.Web;
 
 namespace sccmclictr.automation.functions
 {
+    /// <summary>
+    /// Class processes.
+    /// </summary>
     public class processes : baseInit
     {
         internal Runspace remoteRunspace;
@@ -28,6 +31,12 @@ namespace sccmclictr.automation.functions
         internal ccm baseClient;
 
         //Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="processes"/> class.
+        /// </summary>
+        /// <param name="RemoteRunspace">The remote runspace.</param>
+        /// <param name="PSCode">The PowerShell code.</param>
+        /// <param name="oClient">A CCM CLient object.</param>
         public processes(Runspace RemoteRunspace, TraceSource PSCode, ccm oClient)
             : base(RemoteRunspace, PSCode)
         {
@@ -36,6 +45,10 @@ namespace sccmclictr.automation.functions
             baseClient = oClient;
         }
 
+        /// <summary>
+        /// Gets or sets the win32_ processes.
+        /// </summary>
+        /// <value>The win32_ processes.</value>
         public List<Win32_Process> Win32_Processes
         {
             get
@@ -88,6 +101,11 @@ namespace sccmclictr.automation.functions
 
         }
 
+        /// <summary>
+        /// Extends the processes.
+        /// </summary>
+        /// <param name="Reload">if set to <c>true</c> [reload].</param>
+        /// <returns>List{ExtProcess}.</returns>
         public List<ExtProcess> ExtProcesses(bool Reload)
         {
             return LoadExtProcess(Reload);
@@ -143,6 +161,12 @@ namespace sccmclictr.automation.functions
     public class ExtProcess : Win32_Process
     {
         //Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExtProcess"/> class.
+        /// </summary>
+        /// <param name="WMIObject">The WMI object.</param>
+        /// <param name="RemoteRunspace">The remote runspace.</param>
+        /// <param name="PSCode">The PowerShell code.</param>
         public ExtProcess(PSObject WMIObject, Runspace RemoteRunspace, TraceSource PSCode)
             : base(WMIObject, RemoteRunspace, PSCode)
         {
@@ -158,6 +182,10 @@ namespace sccmclictr.automation.functions
 
         #region Properties
 
+        /// <summary>
+        /// Gets or sets the process owner.
+        /// </summary>
+        /// <value>The process owner.</value>
         public string Owner { get; set; }
 
         #endregion
@@ -170,6 +198,12 @@ namespace sccmclictr.automation.functions
     public class Win32_Process : CIM_Process
     {
         //Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CIM_Process" /> class.
+        /// </summary>
+        /// <param name="WMIObject">The WMI object.</param>
+        /// <param name="RemoteRunspace">The remote runspace.</param>
+        /// <param name="PSCode">The PowerShell code.</param>
         public Win32_Process(PSObject WMIObject, Runspace RemoteRunspace, TraceSource PSCode)
             : base(WMIObject, RemoteRunspace, PSCode)
         {
@@ -222,37 +256,176 @@ namespace sccmclictr.automation.functions
         //internal PSObject WMIObject { get; set; }
         //internal Runspace remoteRunspace;
         //internal TraceSource pSCode;
+
+        /// <summary>
+        /// Gets or sets the command line.
+        /// </summary>
+        /// <value>Command line used to start a specific process, if applicable.</value>
         public String CommandLine { get; set; }
+
+        /// <summary>
+        /// Gets or sets the executable path.
+        /// </summary>
+        /// <value>Path to the executable file of the process.</value>
         public String ExecutablePath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the handle count.
+        /// </summary>
+        /// <value>Total number of open handles owned by the process. HandleCount is the sum of the handles currently open by each thread in this process.</value>
         public UInt32? HandleCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum size of the working set.
+        /// </summary>
+        /// <value>Maximum working set size of the process. The working set of a process is the set of memory pages visible to the process in physical RAM. These pages are resident, and available for an application to use without triggering a page fault.</value>
         public UInt32? MaximumWorkingSetSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum size of the working set.
+        /// </summary>
+        /// <value>Minimum working set size of the process. The working set of a process is the set of memory pages visible to the process in physical RAM. These pages are resident and available for an application to use without triggering a page fault.</value>
         public UInt32? MinimumWorkingSetSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the other operation count.
+        /// </summary>
+        /// <value>Number of I/O operations performed that are not read or write operations.</value>
         public UInt64? OtherOperationCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the other transfer count.
+        /// </summary>
+        /// <value>Amount of data transferred during operations that are not read or write operations.</value>
         public UInt64? OtherTransferCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the page faults.
+        /// </summary>
+        /// <value>Number of page faults that a process generates.</value>
         public UInt32? PageFaults { get; set; }
+
+        /// <summary>
+        /// Gets or sets the page file usage.
+        /// </summary>
+        /// <value>Maximum amount of page file space used during the life of a process.</value>
         public UInt32? PageFileUsage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the parent process identifier.
+        /// </summary>
+        /// <value>Unique identifier of the process that creates a process.</value>
         public UInt32? ParentProcessId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the peak page file usage.
+        /// </summary>
+        /// <value>Maximum amount of page file space used during the life of a process.</value>
         public UInt32? PeakPageFileUsage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the peak virtual.
+        /// </summary>
+        /// <value>Maximum virtual address space a process uses at any one time.</value>
         public UInt64? PeakVirtualSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the peak working set.
+        /// </summary>
+        /// <value>Peak working set size of a process in Kilobytes</value>
         public UInt32? PeakWorkingSetSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the private page count.
+        /// </summary>
+        /// <value>Current number of pages allocated that are only accessible to the process represented by this Win32_Process instance</value>
         public UInt64? PrivatePageCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the process identifier.
+        /// </summary>
+        /// <value>Global process identifier that is used to identify a process. The value is valid from the time a process is created until it is terminated.</value>
         public UInt32? ProcessId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quota non paged pool usage.
+        /// </summary>
+        /// <value>Quota amount of nonpaged pool usage for a process.</value>
         public UInt32? QuotaNonPagedPoolUsage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quota paged pool usage.
+        /// </summary>
+        /// <value>Quota amount of paged pool usage for a process.</value>
         public UInt32? QuotaPagedPoolUsage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quota peak non paged pool usage.
+        /// </summary>
+        /// <value>Peak quota amount of nonpaged pool usage for a process.</value>
         public UInt32? QuotaPeakNonPagedPoolUsage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quota peak paged pool usage.
+        /// </summary>
+        /// <value>Peak quota amount of paged pool usage for a process.</value>
         public UInt32? QuotaPeakPagedPoolUsage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the read operation count.
+        /// </summary>
+        /// <value>Number of read operations performed.</value>
         public UInt64? ReadOperationCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the read transfer count.
+        /// </summary>
+        /// <value>Amount of data read.</value>
         public UInt64? ReadTransferCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the session identifier.
+        /// </summary>
+        /// <value>Unique identifier that an operating system generates when a session is created. A session spans a period of time from logon until logoff from a specific system.</value>
         public UInt32? SessionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the thread count.
+        /// </summary>
+        /// <value>Number of active threads in a process. An instruction is the basic unit of execution in a processor, and a thread is the object that executes an instruction. Each running process has at least one thread. </value>
         public UInt32? ThreadCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the virtual.
+        /// </summary>
+        /// <value>Current size of the virtual address space that a process is using, not the physical or virtual memory actually used by the process.</value>
         public UInt64? VirtualSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the windows version.
+        /// </summary>
+        /// <value>Version of Windows in which the process is running.</value>
         public String WindowsVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets the write operation count.
+        /// </summary>
+        /// <value>Number of write operations performed.</value>
         public UInt64? WriteOperationCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the write transfer count.
+        /// </summary>
+        /// <value>Amount of data written.</value>
         public UInt64? WriteTransferCount { get; set; }
         #endregion
 
         #region Methods
 
+        /// <summary>
+        /// Terminates this instance.
+        /// </summary>
+        /// <returns>UInt32.</returns>
         public UInt32 Terminate()
         {
             //Remove cached results
@@ -301,6 +474,12 @@ namespace sccmclictr.automation.functions
     public class CIM_Process : CIM_LogicalElement
     {
         //Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CIM_Process"/> class.
+        /// </summary>
+        /// <param name="WMIObject">The WMI object.</param>
+        /// <param name="RemoteRunspace">The remote runspace.</param>
+        /// <param name="PSCode">The PowerShell code.</param>
         public CIM_Process(PSObject WMIObject, Runspace RemoteRunspace, TraceSource PSCode)
             : base(WMIObject, RemoteRunspace, PSCode)
         {
@@ -344,19 +523,85 @@ namespace sccmclictr.automation.functions
         //internal PSObject WMIObject { get; set; }
         //internal Runspace remoteRunspace;
         //internal TraceSource pSCode;
+
+        /// <summary>
+        /// Gets or sets the name of the creation class.
+        /// </summary>
+        /// <value>Name of the first concrete class in the inheritance chain that is used to create an instance.</value>
         public String CreationClassName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the creation date.
+        /// </summary>
+        /// <value>Date the process begins executing.</value>
         public DateTime? CreationDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the cs creation class.
+        /// </summary>
+        /// <value>Creation class name of the scoping computer system.</value>
         public String CSCreationClassName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the cs.
+        /// </summary>
+        /// <value>Name of the scoping computer system.</value>
         public String CSName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the state of the execution.
+        /// </summary>
+        /// <value>This property is not implemented and does not get populated for any instance of this class. This property is always NULL.</value>
         public UInt16? ExecutionState { get; set; }
+
+        /// <summary>
+        /// Gets or sets the handle.
+        /// </summary>
+        /// <value>Process identifier.</value>
         public String Handle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the kernel mode time.
+        /// </summary>
+        /// <value>Time in kernel mode, in 100 nanosecond units. If this information is not available, use a value of 0 (zero).</value>
         public UInt64? KernelModeTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the os creation class.
+        /// </summary>
+        /// <value>Creation class name of the scoping operating system.</value>
         public String OSCreationClassName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the os.
+        /// </summary>
+        /// <value>Name of the scoping operating system.</value>
         public String OSName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the priority.
+        /// </summary>
+        /// <value>Scheduling priority of a process within an operating system. The higher the value, the higher priority a process receives. Priority values can range from 0 (zero), which is the lowest priority to 31, which is highest priority.</value>
         public UInt32? Priority { get; set; }
+
+        /// <summary>
+        /// Gets or sets the termination date.
+        /// </summary>
+        /// <value>Process was stopped or terminated. To get the termination time, a handle to the process must be held open. Otherwise, this property returns NULL.</value>
         public DateTime? TerminationDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user mode time.
+        /// </summary>
+        /// <value>Time in user mode, in 100 nanosecond units. If this information is not available, use a value of 0 (zero).</value>
         public UInt64? UserModeTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the working set.
+        /// </summary>
+        /// <value>Amount of memory in bytes that a process needs to execute efficiently—for an operating system that uses page-based memory management.</value>
         public UInt64? WorkingSetSize { get; set; }
+
         #endregion
 
     }
