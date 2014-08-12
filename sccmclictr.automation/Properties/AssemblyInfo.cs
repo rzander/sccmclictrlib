acf37e6d9342555057d4c03f94d2ff5007513bd7
rzander_cp <SND\rzander_cp>
2014-08-12 11:16:50 +0000

    Client Health Check updated to check also for running Applications and Packages (machine only)

--- sccmclictr.automation/Properties/AssemblyInfo.cs
+++ sccmclictr.automation/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -32,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.57")]
-[assembly: AssemblyFileVersion("0.0.0.57")]
+[assembly: AssemblyVersion("0.0.0.58")]
+[assembly: AssemblyFileVersion("0.0.0.58")]
