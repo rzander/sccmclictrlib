42d105dcb5037638368923db96f49daffdc1096a
Roger Zander <roger@zander.ch>
2018-02-03 17:09:45 +0100

    New: delete cached BoundaryGroup

--- sccmclictr.automation/DCM.cs
+++ sccmclictr.automation/DCM.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//SCCM Client Center Automation Library (SCCMCliCtr.automation)
+//Copyright (c) 2018 by Roger Zander
+
+//This program is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 3 of the License, or any later version. 
+//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. 
+//GNU General Public License: http://www.gnu.org/licenses/lgpl.html
+
+using System;
 using System.Collections.Generic;
 using System.Management;
 using System.Management.Automation;
--- sccmclictr.automation/Health.cs
+++ sccmclictr.automation/Health.cs
@@ -1,5 +1,5 @@
 ﻿//SCCM Client Center Automation Library (SCCMCliCtr.automation)
-//Copyright (c) 2011 by Roger Zander
+//Copyright (c) 2018 by Roger Zander
 
 //This program is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 3 of the License, or any later version. 
 //This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. 
@@ -10,14 +10,9 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using sccmclictr.automation;
-using System.Management;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using System.Diagnostics;
-using System.Web;
 
 namespace sccmclictr.automation.functions
 {
